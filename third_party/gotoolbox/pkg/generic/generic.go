f617dc8184f2860d8983381593a003a13fe99855
wwhvw <wangweihong>
2026-07-25 09:05:32 +0800

    feat(gotoolbox): add generic Get func

--- third_party/gotoolbox/pkg/generic/generic.go
+++ third_party/gotoolbox/pkg/generic/generic.go
@@ -19,9 +19,19 @@ type comparable interface {
 	int | int8 | int16 | int32 | int64 | uint | uint8 | uint32 | uint64 | float32 | float64 | string
 }
 
+// GetIfNotNil 如果 k 不为 nil 则返回 *k，否则返回 s
 func GetIfNotNil[T any](s T, k *T) T {
 	if k != nil {
 		return *k
+	}
+	return s
+}
+
+// GetIfNotZero 如果 k 不为零值则返回 k，否则返回 s
+func GetIfNotZero[T comparable](s T, k T) T {
+	var z T
+	if k != z {
+		return k
 	}
 	return s
 }
